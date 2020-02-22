22605c6faace3ed432111211333051a1bbbeb39e
tomo1139 <tomo1139@gmail.com>
2020-02-22 12:11:57 +0900

    プロジェクト作成

---
+++ MediaCodecExtractDecodeEncodeMux/app/src/main/java/develop/tomo1139/mediacodecextractdecodeencodemux/MainActivity.kt
@@ -0,0 +1,12 @@
+package develop.tomo1139.mediacodecextractdecodeencodemux
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
