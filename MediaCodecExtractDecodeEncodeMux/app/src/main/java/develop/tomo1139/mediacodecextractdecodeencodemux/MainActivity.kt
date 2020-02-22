28b29628890295d1072bc69e9a14947d30660317
tomo1139 <tomo1139@gmail.com>
2020-02-22 12:15:51 +0900

    dataBinding導入

--- MediaCodecExtractDecodeEncodeMux/app/src/main/java/develop/tomo1139/mediacodecextractdecodeencodemux/MainActivity.kt
+++ MediaCodecExtractDecodeEncodeMux/app/src/main/java/develop/tomo1139/mediacodecextractdecodeencodemux/MainActivity.kt
@@ -2,11 +2,18 @@ package develop.tomo1139.mediacodecextractdecodeencodemux
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import develop.tomo1139.mediacodecextractdecodeencodemux.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.button.setOnClickListener {
+
+        }
     }
 }
